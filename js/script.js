87d36a09890348b84ab2b5a333090cec4a8bddf8
Daniel Jimenez <ing.danieljimenez521994@hotmail.com>
2021-11-06 01:43:01 -0400

    Modificaciones para incluir mi cv, estilizacion de la imagen de perfil y se agrego la sesion acerca de mi.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //
 
-const getProfile = () => {
-    const btn = document.getElementById('btn');
-};
+const btnHeroesProject = document.getElementById('btnHeroesProject');
+
+btnHeroesProject.addEventListener('click', () => {
+    alert('Funcionalidad en proceso de desarrollo');
+});
+
+// const getProfile = () => {
+//     const btn = document.getElementById('btn');
+// };
