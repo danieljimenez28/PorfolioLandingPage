f1e3e60d915f028d65f34b46c97f3a7059f72482
Daniel Jimenez <ing.danieljimenez521994@hotmail.com>
2021-11-12 06:57:55 -0400

    Verificacion de errores comules, vista del cv y otras mejoras

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 //
 
 const btnHeroesProject = document.getElementById('btnHeroesProject');
+const btnViewCV = document.getElementById('btnViewCV');
+const fileContainer = document.getElementById('file-container');
+const closePDF = document.getElementById('closePDF');
+
+
 
 btnHeroesProject.addEventListener('click', () => {
     alert('Funcionalidad en proceso de desarrollo');
 });
 
-// const getProfile = () => {
-//     const btn = document.getElementById('btn');
-// };
+btnViewCV.addEventListener('click', () => {
+    fileContainer.classList.add('file-container-display');
+});
+
+closePDF.addEventListener('click', () => {
+    fileContainer.classList.remove('file-container-display');
+});
